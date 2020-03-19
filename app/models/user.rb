96d2cc61f9c821fc77e7ea93311b63cec3d2aaa9
Takanori <kaku.takanori.1026@gmail.com>
2020-03-19 15:28:52 +0000

    user_model_finished

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  
+  validates :password, presence: true, length: {minimum: 8}, on: :facebook_login
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  def self.from_omniauth(auth)
+    user = User.where('email = ?', auth.info.email).first
+    if user.blank?
+       user = User.new
+    end
+    user.uid   = auth.uid
+    user.username  = auth.info.name
+    user.email = auth.info.email
+    user.oauth_token = auth.credentials.token
+    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  
+  get '/auth/:provider/callback',    to: 'users#create',       as: :auth_callback
+  get '/auth/failure',               to: 'users#auth_failure', as: :auth_failure
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

38e00d72cd91fe7c5138dce8a1b16ced722241a5
Takanori <kaku.takanori.1026@gmail.com>
2020-04-20 15:12:17 +0000

    enum_定義_enum_help_導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,19 @@
 class OrdersController < ApplicationController
   
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
   
   def new
+    @order = Order.new
   end
   
   def create
+    @order = @user.orders.build(order_params)
+    if @order.save
+      flash[:notice] = "技工指示書を新規作成しました。"
+      redirect_to @user
+    else
+      render :new
+    end
   end
   
   def show
@@ -24,6 +32,11 @@ class OrdersController < ApplicationController
   end
   
   private
+  
+    def order_params
+      params.require(:order).permit(:patient_name, :sex, :color, :note, :content, :crown, :metal, :first_try, :second_try,
+                                    :complete_day, :reception_date, :weight, :finished)
+    end                                
     
     def set_user
       @user = User.find(params[:user_id])
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,6 @@ class Order < ApplicationRecord
   validates :patient_name, presence: true
   validates :sex, presence: true
   validates :note, presence: true, length: { maximum: 100 }
+  
+  enum color: { a1: 0, a2: 1, a3: 2, a35: 3, a4: 4, photo: 5 }
 end

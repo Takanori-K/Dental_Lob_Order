20fcce80b871891736e7343f504cfc29af7b50ac
Takanori <kaku.takanori.1026@gmail.com>
2020-04-23 16:23:14 +0000

    order-image30%完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,7 +35,7 @@ class OrdersController < ApplicationController
   
     def order_params
       params.require(:order).permit(:patient_name, :sex, :color, :note, :content, :crown, :metal, :first_try, :second_try,
-                                    :complete_day, :reception_date, :weight, :finished)
+                                    :complete_day, :reception_date, :weight, :finished, :image_1, :image_2, :image_3)
     end                                
     
     def set_user
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
-  
+  mount_uploader :image_1, ImageUploader
+  mount_uploader :image_2, ImageUploader
+  mount_uploader :image_3, ImageUploader
   validates :patient_name, presence: true
   validates :sex, presence: true
   validates :note, presence: true, length: { maximum: 100 }

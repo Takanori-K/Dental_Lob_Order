1b81ccbdced8ea6720b9f741c1df7fb8e9eb3619
Takanori <kaku.takanori.1026@gmail.com>
2020-04-24 17:31:21 +0000

    order_new_view_ja_完了

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,10 @@ class Order < ApplicationRecord
   mount_uploader :image_2, ImageUploader
   mount_uploader :image_3, ImageUploader
   validates :patient_name, presence: true
-  validates :sex, presence: true
-  validates :note, presence: true, length: { maximum: 100 }
-  
+  validates :sex,            presence: true
+  validates :note,           presence: true, length: { maximum: 100 }
+  validates :content,        presence: true
+  validates :crown ,         presence: true
+  validates :reception_date, presence: true
   enum color: { a1: 0, a2: 1, a3: 2, a35: 3, a4: 4, photo: 5 }
 end

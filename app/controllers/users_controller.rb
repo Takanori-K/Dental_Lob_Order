d78151898e88d0b88ff130323241741765ac1868
Takanori <kaku.takanori.1026@gmail.com>
2020-05-11 15:35:01 +0000

    user_image_プレビュー完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :remove_image)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache, :remove_image)
     end
     
     def set_user

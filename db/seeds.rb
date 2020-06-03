510e17ee5bf406c98756172bd4d66ed28511597c
Takanori <kaku.takanori.1026@gmail.com>
2020-06-03 04:53:28 +0000

    order_index_検索再完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "テストデンタルラボラトリー",
-             email: "kaku.takanori.1026@gmail.com",
+User.create!(name: "テストラボラトリー",
+             email: "admin@email.com",
              password: "password",
              password_confirmation: "password",
              admin: true)

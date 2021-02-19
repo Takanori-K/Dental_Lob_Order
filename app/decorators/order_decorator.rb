4673d8ad4e0b7be18c1a6ab7868de731fda6c62b
takanori_kaku <kaku.takanori.1026@gmail.com>
2021-02-20 00:42:56 +0900

    orderモデルの画像表示をデコレターに変更

--- app/decorators/order_decorator.rb
+++ app/decorators/order_decorator.rb
@@ -1,4 +1,53 @@
 # frozen_string_literal: true
 
 module OrderDecorator
+
+  def order_image_1_edit
+    if image_1?
+      image_tag image_1.thumb.url, id: :edit_img_prev, onClick:"$('#upfile').click()"
+    else
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg", id: :edit_img_prev, onClick:"$('#upfile').click()"
+    end
+  end
+
+  def order_image_2_edit
+    if image_2?
+      image_tag image_2.thumb.url, id: :edit_img_prev, onClick:"$('#upfile').click()"
+    else
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg", id: :edit_img_prev, onClick:"$('#upfile').click()"
+    end
+  end
+
+  def order_image_3_edit
+    if image_3?
+      image_tag image_3.thumb.url, id: :edit_img_prev, onClick:"$('#upfile').click()"
+    else
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg", id: :edit_img_prev, onClick:"$('#upfile').click()"
+    end
+  end
+
+  def order_image_1
+    if image_1?
+      image_tag image_1.thumb.url
+    else
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg"
+    end
+  end
+
+  def order_image_2
+    if image_2?
+      image_tag image_2.thumb.url
+    else
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg"
+    end
+  end
+
+  def order_image_3
+    if image_3?
+      image_tag image_3.thumb.url
+    else
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg"
+    end
+  end
+
 end
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,12 +2,20 @@
 
 module UserDecorator
 
-  def user_image
+  def user_image_edit
     if image?
       image_tag image.thumb.url, id: :img_prev, onClick:"$('#user_img').click()"
     else
       image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/facility_default.png", id: :img_prev, onClick:"$('#user_img').click()"
     end
   end
-  
+
+  def user_image
+    if image?
+      image_tag image.thumb.url
+    else
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/facility_default.png"
+    end
+  end
+
 end

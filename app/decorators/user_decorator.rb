7b1065b207a7934dda6bf37d288993db19240b2a
takanori_kaku <kaku.takanori.1026@gmail.com>
2021-02-20 00:07:07 +0900

    userモデルの画像表示をデコレーターに移動

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module UserDecorator
+
+  def user_image
+    if image?
+      image_tag image.thumb.url, id: :img_prev, onClick:"$('#user_img').click()"
+    else
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/facility_default.png", id: :img_prev, onClick:"$('#user_img').click()"
+    end
+  end
+  
 end

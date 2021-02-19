5ab106f9e821a7979a2c1ffd0742d8c262be435f
takanori_kaku <kaku.takanori.1026@gmail.com>
2021-02-20 01:20:56 +0900

    デコレーター修正、rubocop導入

--- app/decorators/order_decorator.rb
+++ app/decorators/order_decorator.rb
@@ -12,17 +12,17 @@ module OrderDecorator
 
   def order_image_2_edit
     if image_2?
-      image_tag image_2.thumb.url, id: :edit_img_prev, onClick:"$('#upfile').click()"
+      image_tag image_2.thumb.url, id: :edit_img_prev_2, onClick:"$('#upfile2').click()"
     else
-      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg", id: :edit_img_prev, onClick:"$('#upfile').click()"
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg", id: :edit_img_prev_2, onClick:"$('#upfile2').click()"
     end
   end
 
   def order_image_3_edit
     if image_3?
-      image_tag image_3.thumb.url, id: :edit_img_prev, onClick:"$('#upfile').click()"
+      image_tag image_3.thumb.url, id: :edit_img_prev_3, onClick:"$('#upfile3').click()"
     else
-      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg", id: :edit_img_prev, onClick:"$('#upfile').click()"
+      image_tag "https://takanori-private-image.s3-ap-northeast-1.amazonaws.com/background-image/no-image.jpg", id: :edit_img_prev_3, onClick:"$('#upfile3').click()"
     end
   end
 

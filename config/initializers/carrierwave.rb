7f0024f8328beed5f4f60f4d68c8b42be94bf3a5
takanori_kaku <kaku.takanori.1026@gmail.com>
2021-03-24 17:08:54 +0900

    S3に画像アップロード

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = ENV['AWS_S3_BUCKET'] # バケット名
-    config.asset_host = 'https://takanori-private-image.s3.amazonaws.com'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/takanori-private-image'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
